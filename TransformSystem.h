cea66ea02bc9ee887c4028ecf8348761b187684e
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-05-26 08:37:12 -0400

    Added TransformSystem

---
+++ TransformSystem.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Transform.h"
+#include "glm/glm.hpp"
+class TransformSystem
+{
+public:
+	static void MoveRelative(glm::vec3 unrotated, Transform*);
+	static void Rotate(glm::vec3 rotation, Transform*);
+	static void SetOrientation(glm::vec3 rotation, Transform*);
+	static void CalculateWorldMatrix(Transform* transform);
+	static glm::vec3 CalculateForward(Transform* transform);
+	static void CalculatePosition(Transform& transform);
+	static void CalculateEulerAngles(Transform& transform);
+	static void CalculateScale(Transform& transform);
+	static glm::vec3 GetEulerRotation(Transform& transform);
+	static void DecomposeTransform(const Transform& toDecomp, Transform& out);
+};
+
