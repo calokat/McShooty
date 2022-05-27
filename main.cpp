762638e87aff0e386a866847e1a6fce8efe36c6b
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-05-27 10:52:20 -0400

    Linux event loop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#ifdef _WIN64
 #include "WindowsPlatform.h"
+#endif
+#ifdef __linux__
+#include "LinuxPlatform.h"
+#endif
 #include "OpenGLAPI.h"
 #include "MeshLoaderSystem.h"
 #include "Shader.h"
@@ -13,10 +18,25 @@
 #define SetupAttribute(index, size, type, structure, element) \
 	glVertexAttribPointer(index, size, type, 0, sizeof(structure), (void*)offsetof(structure, element)); \
 
+bool EventLoop()
+{
+    SDL_Event e;
+    while (SDL_PollEvent(&e))
+    {
+        if (e.type == SDL_KEYDOWN)
+        {
+            if (e.key.keysym.scancode == SDL_Scancode::SDL_SCANCODE_ESCAPE)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 int main(int argc, char* argv[])
 {
-    WindowsPlatform platform;
+    LinuxPlatform platform;
     OpenGLAPI graphics(&platform);
     platform.InitWindow();
     graphics.Init();
@@ -27,14 +47,15 @@ int main(int argc, char* argv[])
     TransformSystem::CalculateWorldMatrix(&cameraTransform);
     CameraSystem::CalculateViewMatrixLH(camera, cameraTransform);
     CameraSystem::CalculateProjectionMatrixLH(camera, camera.aspectRatio);
-    MeshLoaderSystem::ProcessMesh("C:\\Users\\Caleb\\Documents\\GitHub\\PortableEngine\\Assets\\Models\\helix.obj", spiral.mesh);
+    MeshLoaderSystem::ProcessMesh("~/git/PortableEngine/Assets/Models/helix.obj", spiral.mesh);
     spiral.mesh.renderData = std::make_unique<MeshOpenGLRenderData>();
     OpenGLRenderSystem renderSystem;
     renderSystem.LoadRenderCameraParams(camera);
     spiral.transform.position = glm::vec3(0, 0, 15);
     TransformSystem::CalculateWorldMatrix(&spiral.transform);
     renderSystem.InstantiateRenderedObject(spiral);
-    while (!(GetAsyncKeyState(VK_ESCAPE) & 0x8000)) 
+    
+    while (EventLoop())
     {
         platform.Run();
         graphics.ClearScreen();
--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #pragma once
+#ifdef _WIN64
 #include <Windows.h>
-
-int InitWindow();
 LRESULT EventLoop(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+#endif
+#ifdef __linux__
+#include <SDL2/SDL.h>
+bool EventLoop();
+#endif
+int InitWindow();
 long Run();
