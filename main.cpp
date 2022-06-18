acaa1feaf146cd7e1dcb327e13d1431c68230eef
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-06-18 14:47:52 -0400

    Implemented rainbow rendering, right hand tracking, spiral spinning

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #include "OpenGLRenderSystem.h"
 #include "OpenXrApi.h"
 #include "SteamVrApi.h"
+#include <chrono>
 
 #define SetupAttribute(index, size, type, structure, element) \
 	glVertexAttribPointer(index, size, type, 0, sizeof(structure), (void*)offsetof(structure, element)); \
@@ -36,14 +37,17 @@ int main(int argc, char* argv[])
     OpenGLAPI graphics(winGlContext);
     graphics.Init();
     printf("Welcome to McShooty's\n");
-    //OpenXrApi xr(platform, graphics, PE::GraphicsAPI::OpenGL);
-    SteamVrApi vr;
+    OpenXrApi xr(platform, graphics, PE::GraphicsAPI::OpenGL);
+    //SteamVrApi vr;
     //RenderedObject spiral, torus;
-    std::vector<RenderedObject> objectsToRender(2);
+    std::vector<RenderedObject> objectsToRender(3);
     RenderedObject& spiral = objectsToRender[0];
     RenderedObject& leftHand = objectsToRender[1];
+    RenderedObject& rightHand = objectsToRender[2];
     spiral.renderer.colorTint = { 1, 0, 1, 1 };
+    spiral.transform.scale = glm::vec3(5);
     leftHand.renderer.colorTint = { 0, 0, 0, 1 };
+    rightHand.renderer.colorTint = { 1, 1, 1, 1 };
     Camera camera(8.0f / 6);
     Transform cameraTransform; 
     TransformSystem::CalculateWorldMatrix(&cameraTransform);
@@ -57,24 +61,42 @@ int main(int argc, char* argv[])
     MeshLoaderSystem::ProcessMesh("Models\\helix.obj", *spiralMesh);
     spiral.mesh = spiralMesh;
     leftHand.mesh = handMesh;
+    rightHand.mesh = handMesh;
     OpenGLRenderSystem renderSystem;
     renderSystem.LoadRenderCameraParams(camera);
     spiral.transform.orientation = glm::quat(glm::vec3(0, 0, glm::radians(90.0f)));
 
     TransformSystem::CalculateWorldMatrix(&spiral.transform);
     TransformSystem::CalculateWorldMatrix(&leftHand.transform);
+    TransformSystem::CalculateWorldMatrix(&rightHand.transform);
     renderSystem.InstantiateRenderedObject(spiral);
     renderSystem.InstantiateRenderedObject(leftHand);
+    renderSystem.InstantiateRenderedObject(rightHand);
 
+
+    // with thanks to https://stackoverflow.com/questions/728068/how-to-calculate-a-time-difference-in-c
+    double elapsed_time_ms = 0;
+    float totalTime = 0;
     while (platform.Run())
     {
-        vr.UpdateDevices();
-        leftHand.transform = vr.GetTrackedDeviceTransform(ETrackedDeviceType::LeftHandController);
-        vr.DrawFrame(objectsToRender, renderSystem);
+        auto t_start = std::chrono::high_resolution_clock::now();
+        if (totalTime > 10) totalTime -= 10;
+        spiral.renderer.time = totalTime;
+        leftHand.renderer.time = totalTime;
+        rightHand.renderer.time = totalTime;
+        //vr.UpdateDevices();
+        //leftHand.transform = vr.GetTrackedDeviceTransform(ETrackedDeviceType::LeftHandController);
+        //rightHand.transform = vr.GetTrackedDeviceTransform(ETrackedDeviceType::RightHandController);
+        spiral.transform.orientation = spiral.transform.orientation * glm::angleAxis((float)elapsed_time_ms / 1000, glm::vec3(0, 1, 0));
+        TransformSystem::CalculateWorldMatrix(&spiral.transform);
+        //vr.DrawFrame(objectsToRender, renderSystem);
+        xr.Frame(objectsToRender, renderSystem, cameraTransform);
         graphics.ClearScreen();
-        renderSystem.Draw(spiral);
-        graphics._SwapBuffers();
-        //xr.Frame(objectsToRender, renderSystem, cameraTransform);
+        //renderSystem.Draw(spiral);
+        //graphics._SwapBuffers();
+        auto t_end = std::chrono::high_resolution_clock::now();
+        elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end - t_start).count();
+        totalTime += (elapsed_time_ms / 1000.0f);
     }
     return 0;
 }
